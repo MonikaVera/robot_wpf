528361fb008056321574afd2c936887607fe9b19
Mathe Arnold <l6u85u@inf.elte.hu>
2023-04-08 00:19:54 +0300

    ViewModelField.cs

--- View/ViewModel/ViewModelField.cs
+++ View/ViewModel/ViewModelField.cs
@@ -9,18 +9,56 @@ namespace View.ViewModel
 {
     public class ViewModelField : ViewModelBase
     {
+        #region Fields
         private int _indX;
         private int _indY;
         private Color _color;
         private string? _picture;
         private string? _text;
         private int _number;
+        #endregion
+
+        #region Properties
         public Color setColor { get { return _color; } set { _color = value; } }
-        public int IndY { get { return _indY; } set { _indY = value; } }
-        public int IndX { get { return _indX; } set { _indX = value; } }
+        public int IndY
+        {
+            get { return _indY; }
+            set
+            {
+                if (_indY != value)
+                {
+                    _indY = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int IndX
+        {
+            get { return _indX; }
+            set
+            {
+                if (_indX != value)
+                {
+                    _indX = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        
         public string? Picture { get { return _picture; } set { _picture = value; } }
         public int Number { get { return _number; } set { _number = value; } }
-        public string? Text { get { return _text; } set { _text = value; } }
+        public string? Text {
+            get { return _text; }
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
         public void SetText(Field field)
         {
@@ -31,6 +69,10 @@ namespace View.ViewModel
             else if (field is Obstacle)
             {
                 _text = ("O");
+            }
+            else if (field is Cube)
+            {
+                _text = ("C");
             }
             else if (field is Exit)
             {
@@ -45,5 +87,7 @@ namespace View.ViewModel
         /// Mezőváltoztató parancs lekérdezése, vagy beállítása.
         /// </summary>
         public DelegateCommand? FieldChangeCommand { get; set; }
+
+        #endregion
     }
 }

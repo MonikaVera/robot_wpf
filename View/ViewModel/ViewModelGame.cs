a056bc062a5f4b61b52b18ae319b4ce8103f00d3
Mathe Arnold <l6u85u@inf.elte.hu>
2023-04-09 13:36:06 +0300

    ViewModelGame.cs

--- View/ViewModel/ViewModelGame.cs
+++ View/ViewModel/ViewModelGame.cs
@@ -18,27 +18,22 @@ namespace View.ViewModel
         public DelegateCommand PlayerModeCommand { get; private set; }
         public DelegateCommand ViewerModeCommand { get; private set; }
         public DelegateCommand ExitCommand { get; private set; }
+        public DelegateCommand KeyDownCommand { get; private set; }
+        public DelegateCommand ChooseActionFieldCommand { get; private set; }
 
         public DelegateCommand NewGameCommand { get; private set; }
         public DelegateCommand SaveGameCommand { get; private set; }
         public DelegateCommand LoadGameCommand { get; private set; }
         public DelegateCommand ExitGameCommand { get; private set; }
         public DelegateCommand NewExtraTask { get; private set; }
-        public DelegateCommand PressUp { get; private set; }
-        public DelegateCommand PressDown { get; private set; }
-        public DelegateCommand PressRight { get; private set; }
-        public DelegateCommand PressLeft { get; private set; }
-        public DelegateCommand ConnectRobot { get; private set; }
-        public DelegateCommand DisConnectRobot { get; private set; }
-        public DelegateCommand ConnectCubes { get; private set; }
-        public DelegateCommand DisConnectCubes { get; private set; }
-        public DelegateCommand RotateRobot { get; private set; }
+        
         #endregion
 
         #region Events
         public event EventHandler? PlayerModeClick;
         public event EventHandler? ViewerModeClick;
         public event EventHandler? ExitClick;
+        
 
         public event EventHandler ExitGame;
         public event EventHandler LoadGame;
@@ -62,10 +57,21 @@ namespace View.ViewModel
             }
         }
 
+        public int Round
+        {
+            get { return _model.Round; }
+            set
+            {
+                OnPropertyChanged(nameof(Round));
+            }
+        }
+
+
+
         public Game Game { get; set; }
 
         public String GameTime { get { return TimeSpan.FromSeconds(_model.GameTime).ToString("g"); } }
-        public Int32 Round { get { return 1; } }//_model.Turns; } }
+      
 
         //  public int LocX { get { return _model.LocX; } }
         // public int LocY { get { return _model.LocY; } }
@@ -79,7 +85,8 @@ namespace View.ViewModel
             PlayerModeCommand = new DelegateCommand(param => OnPlayerModeClick());
             ViewerModeCommand = new DelegateCommand(param => OnViewerModeClick());
             ExitCommand = new DelegateCommand(param => OnExitClick());
-
+            KeyDownCommand = new DelegateCommand(param => KeyDown(Convert.ToString(param)));
+            
 
             //jatektabla letrehozasa
             Fields = new ObservableCollection<ViewModelField>();
@@ -88,8 +95,11 @@ namespace View.ViewModel
                 {
                     ViewModelField field = new ViewModelField();
                     field.SetText(_model.Board.GetFieldValue(i, j));
+                    field.Number = j * _model.Board.Width + i;
                     field.IndX = i;
                     field.IndY = j;
+                    field.ChooseActionFieldCommand = new DelegateCommand(param => ChooseActionField(Convert.ToInt32(param)));
+
                     Fields.Add(field);
                 }
 
@@ -111,12 +121,76 @@ namespace View.ViewModel
             ExitClick?.Invoke(this, EventArgs.Empty);
         }
 
+        private void ChooseActionField(int number)
+        {
+            ViewModelField field = Fields[number];
+
+            _model.ChooseActionField(field.IndX, field.IndY);
+        }
+
+        private void KeyDown(string? action)
+        {
+            // lekérdezzük, hogy hol helyezkedik épp a robot
+            Int32 x = _model.Robot.X;
+            Int32 y = _model.Robot.Y;
+
+
+            if (action == "RIGHT" && (x + 1) < _model.Board.Width) // jobbra lépünk
+            {
+                _model.MoveRobot(_model.Robot, Direction.EAST); 
+            }
+            else if (action == "LEFT" && (x - 1) >= 0) // balra lépünk
+            {
+                _model.MoveRobot(_model.Robot, Direction.WEST); 
+            }
+            else if (action == "UP" && (y - 1) >= 0) // felfele lépünk
+            {
+                _model.MoveRobot(_model.Robot,Direction.NORTH); 
+            }
+            else if (action == "DOWN" && (y + 1) < _model.Board.Height) // lefele lépünk
+            {
+                _model.MoveRobot(_model.Robot,Direction.SOUTH); 
+            }
+            else if (action == "CLEAN") // tisztitunk
+            {
+                _model.Clean(_model.Robot); 
+            }
+            else if (action == "MERGE") // osszekapcsolunk kockakat
+            {
+                _model.ConnectCubes(_model.Robot); 
+            }
+            else if (action == "GET") // rakapcsolodunk egy kockara
+            {
+                _model.ConnectRobot(_model.Robot); 
+            }
+            else if (action == "PUTDOWN") // lekapcsolodunk egy kockarol
+            {
+                _model.DisconnectRobot(_model.Robot);
+            }
+            else if (action == "SPLIT") // szetkapcsolunk kockakat
+            {
+                _model.DisconnectCubes(_model.Robot);
+            }
+            else if (action == "TURNEAST") // oramutatoval megegyezo iranyu forgas
+            {
+                _model.RotateRobot(_model.Robot, Angle.Clockwise);
+            }
+            else if (action == "TURNWEST") // oramutatoval ellentetes iranyu forgas
+            {
+                _model.RotateRobot(_model.Robot, Angle.CounterClockwise);
+            }
+            else if (action == "WAIT") // varakozas
+            {
+                _model.Wait(_model.Robot);
+            }
+
+
+            OnPropertyChanged(nameof(Round));
+        }
+
         private void Model_GameAdvanced(object? sender, GameEventArgs e)
         {
             OnPropertyChanged(nameof(GameTime));
-            OnPropertyChanged(nameof(Round));
-            //  OnPropertyChanged(nameof(LocX));
-            //  OnPropertyChanged(nameof(LocY));
         }
 
         /// <summary>
@@ -135,15 +209,7 @@ namespace View.ViewModel
         private void Model_ConnectCubes(object obj, RobotEventArgs e) {/*code*/; }
         private void Model_DisConnectCubes(object obj, RobotEventArgs e) {/*code*/; }
         private void Model_Clean(object obj, RobotEventArgs e) {/*code*/; }
-        private void OnPressUp() {/*code*/; }
-        private void OnPressDown() {/*code*/; }
-        private void OnPressRight() {/*code*/; }
-        private void OnPressLeft() {/*code*/; }
-        private void OnConnectRobot() {/*code*/; }
-        private void OnDisConnectRobot() {/*code*/; }
-        private void OnConnectCubes() {/*code*/; }
-        private void OnDisConnectCubes() {/*code*/; }
-        private void OnRotateRobot() {/*code*/; }
+       
         private void ReFresh() {/*code*/; }
         private void OnLoadGame() {/*code*/; }
         private void OnNewGame() {/*code*/; }

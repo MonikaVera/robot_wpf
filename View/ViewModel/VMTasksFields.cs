efc00904e01ff822709bc867b002ac4794a98c11
Heni <jurcahh@gmail.com>
2023-04-11 22:28:08 +0200

    fixing_VM

--- View/ViewModel/VMTasksFields.cs
+++ View/ViewModel/VMTasksFields.cs
@@ -9,25 +9,15 @@ namespace View.ViewModel
 {
     public class VMTasksFields : ViewModelBase
     {
-        private String cubeColor = String.Empty;
         private string? _text;
+        private String? _image = null;
+        private Color _color;
         private int _number;
 
         /// <summary>
         /// Játékos lekérdezése, vagy beállítása.
         /// </summary>
-        public String CubeColor
-        {
-            get { return cubeColor; }
-            set
-            {
-                if (cubeColor != value)
-                {
-                    cubeColor = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
+        public Color setColor { get { return _color; } set { _color = value; } }
 
         /// <summary>
         /// Oszlop lekérdezése, vagy beállítása.
@@ -59,22 +49,12 @@ namespace View.ViewModel
             {
                 _text = " ";
             }
-            else if (field is Obstacle)
-            {
-                _text = ("O");
-            }
             else if (field is Cube)
             {
+                //_image = "cube.jpg";
                 _text = ("C");
             }
-            else if (field is Exit)
-            {
-                _text = ("E");
-            }
-            else if (field is Robot)
-            {
-                _text = ("R");
-            }
+
             OnPropertyChanged(nameof(Text));
         }
 
